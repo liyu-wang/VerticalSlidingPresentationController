1a5b3d780cb5b695226148abfd84276e7c2a82d2
Liyu Wang <liyuwang1985@gmail.com>
2020-11-15 22:01:08 +1100

    Added the initial project.

---
+++ VerticalSlidingPresentationController/VerticalSlidingPresentationController.h
@@ -0,0 +1,18 @@
+//
+//  VerticalSlidingPresentationController.h
+//  VerticalSlidingPresentationController
+//
+//  Created by Liyu Wang on 15/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VerticalSlidingPresentationController.
+FOUNDATION_EXPORT double VerticalSlidingPresentationControllerVersionNumber;
+
+//! Project version string for VerticalSlidingPresentationController.
+FOUNDATION_EXPORT const unsigned char VerticalSlidingPresentationControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VerticalSlidingPresentationController/PublicHeader.h>
+
+
