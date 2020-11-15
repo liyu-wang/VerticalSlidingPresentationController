57e37d7f29973395c446f162d7679f08a693316c
Liyu Wang <liyuwang1985@gmail.com>
2020-11-15 23:03:25 +1100

    Added the target for the demo app.

---
+++ VerticalSlidingPresentationControllerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VerticalSlidingPresentationControllerDemo
+//
+//  Created by Liyu Wang on 15/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
